4bda7bcea6ddf99ab0b13018f0b8f81ebbc42cd1
Anthony Timmins <atimmins@datto.com>
2022-04-14 17:48:10 -0400

    Testing

--- src/networkd.c
+++ src/networkd.c
@@ -449,7 +449,7 @@ write_netdev_file(const NetplanNetDefinition* def, const char* rootdir, const ch
             break;
 
         case NETPLAN_DEF_TYPE_VXLAN:
-            g_string_append_printf(s, "Kind=vxlan\n\n[VXLAN]\nVNI=%u\n", def->vxlan_id);
+            g_string_append_printf(s, "Kind=vxlan\n\n[VXLAN]\nVNI=%u", def->vxlan_id);
             write_vxlan_parameters(def, s);
             break;
 
