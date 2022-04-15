5498a875c0d83bcf107fd0c6a4f82e1f07999635
Anthony Timmins <atimmins@datto.com>
2022-04-15 00:30:44 -0400

    Testing

--- src/netplan.c
+++ src/netplan.c
@@ -781,7 +781,7 @@ _serialize_yaml(
         YAML_SCALAR_PLAIN(event, emitter, "vxlans");
         YAML_SEQUENCE_OPEN(event, emitter);
         for (unsigned i = 0; i < def->vxlans->len; ++i) {
-            YAML_SCALAR_PLAIN(event, emitter, def->vxlans);
+            YAML_SCALAR_PLAIN(event, emitter, g_array_index(def->vxlans, char*, i));
         }
         YAML_SEQUENCE_CLOSE(event, emitter);
     }
