112e206bbb8eda89b75a38ef4c2a92a81e10b899
Anthony Timmins <atimmins@datto.com>
2022-04-14 23:59:44 -0400

    Testing

--- src/netplan.c
+++ src/netplan.c
@@ -660,7 +660,7 @@ write_vxlans(yaml_event_t* event, yaml_emitter_t* emitter, const NetplanNetDefin
     YAML_SCALAR_PLAIN(event, emitter, "vxlans");
     YAML_MAPPING_OPEN(event, emitter);
     if (def->vxlans){
-        YAML_SCALAR_PLAIN(event, emitter, "vxlans");
+        YAML_SCALAR_PLAIN(event, emitter, "vxlan");
         YAML_SEQUENCE_OPEN(event, emitter);
         for (unsigned i = 0; i < def->vxlans->len; ++i)
             YAML_SCALAR_PLAIN(event, emitter, g_array_index(def->vxlans, char*, i));
