0f9f96ddcfa7936d907231cef0b25f77845ef389
Anthony Timmins <atimmins@datto.com>
2022-04-15 00:17:14 -0400

    Testing

--- src/netplan.c
+++ src/netplan.c
@@ -662,6 +662,7 @@ write_vxlans(yaml_event_t* event, yaml_emitter_t* emitter, const NetplanNetDefin
     if (def->vxlans) {
         for (unsigned i = 0; i < def->vxlans->len; ++i)
             YAML_SCALAR_QUOTED(event, emitter, g_array_index(def->vxlans, char*, i));
+    }
     YAML_SEQUENCE_CLOSE(event, emitter);
     return TRUE;
 err_path: return FALSE; // LCOV_EXCL_LINE
