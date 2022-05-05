d205ca56f30131f15c9dafb07dfffce266610971
Anthony Timmins <atimmins@datto.com>
2022-05-05 19:16:05 -0400

    Testing

--- src/validation.c
+++ src/validation.c
@@ -336,6 +336,13 @@ validate_netdef_grammar(const NetplanParser* npp, NetplanNetDefinition* nd, yaml
             return yaml_error(npp, node, error, "%s: invalid ttl '%u' (allowed values are 0 to 255)", nd->id, nd->vxlan_params.ttl);
         if (nd->vxlan_params.flow_label > 1048575)
             return yaml_error(npp, node, error, "%s: invalid flow-label '%u' (allowed values are 0 to 1048575)", nd->id, nd->vxlan_params.flow_label);
+        if (nd->vxlan_params.source_port_range) {
+            for (guint i = 0; i < nd->vxlan_params.source_port_range->len; i++) {
+                char lower = g_array_index (nd->vxlan_params.source_port_range, char*, i);
+                char upper = g_array_index (nd->vxlan_params.source_port_range, char*, ++i);
+                return yaml_error(npp, node, error, "%s: invalid source-port-range '%u' %s %s", nd->id, nd->vxlan_params.flow_label, lower, upper);
+            }
+        }
     }
 
     if (nd->type == NETPLAN_DEF_TYPE_VRF) {
