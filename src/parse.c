0944c13a979adab03cc2a8bba4d1f494f15525f9
Anthony Timmins <atimmins@datto.com>
2022-05-05 19:02:21 -0400

    Testing

--- src/parse.c
+++ src/parse.c
@@ -2014,13 +2014,14 @@ handle_vrf_interfaces(NetplanParser* npp, yaml_node_t* node, const void* data, G
 static gboolean
 handle_vxlan_source_port(NetplanParser* npp, yaml_node_t* node, const void* data, GError** error)
 {
-    if (!npp->current.netdef->vxlan_params.source_port_range) {
-        npp->current.netdef->vxlan_params.source_port_range = g_array_new(FALSE, FALSE, sizeof(char *));
-    }
-
     for (yaml_node_item_t *i = node->data.sequence.items.start; i < node->data.sequence.items.top; i++) {
         yaml_node_t *entry = yaml_document_get_node(&npp->doc, *i);
         assert_type(npp, entry, YAML_SCALAR_NODE);
+
+        if (!npp->current.netdef->vxlan_params.source_port_range) {
+            npp->current.netdef->vxlan_params.source_port_range = g_array_new(FALSE, FALSE, sizeof(char *));
+            break;
+        }
         char* s = g_strdup(scalar(entry));
         g_array_append_val(npp->current.netdef->vxlan_params.source_port_range, s);
     }
