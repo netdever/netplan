c8ded989c731ced73ef794292cb63553dcb87e7f
Anthony Timmins <atimmins@datto.com>
2022-05-05 15:40:55 -0400

    Testing

--- src/networkd.c
+++ src/networkd.c
@@ -430,10 +430,12 @@ write_vxlan_parameters(const NetplanNetDefinition* def, GString* s)
         g_string_append_printf(params, "\nGenericProtocolExtension=%s", def->vxlan_params.generic_protocol_extension ? "True" : "False");
     if (def->vxlan_params.destination_port)
         g_string_append_printf(params, "\nDestinationPort=%d", def->vxlan_params.destination_port);
-    if (def->vxlan_params.source_port_range) {
+    if (def->vxlan_params.source_port_range && def->vxlan_params.source_port_range->len > 0) {
+        g_string_append_printf(params, "\nPortRange=");
         for (unsigned i = 0; i < def->vxlan_params.source_port_range->len; ++i) {
-            g_string_append_printf(params, "\nPortRange=%s", "BANANA!");
-            g_string_append_printf(params, "\nPortRange=%s", g_array_index(def->vxlan_params.source_port_range, char*, i));
+            if (i > 0)
+                g_string_append_printf(params, " ");
+            g_string_append_printf(params, "%s", g_array_index(def->vxlan_params.source_port_range, char*, i));
         }
     }
     if (def->vxlan_params.flow_label)
