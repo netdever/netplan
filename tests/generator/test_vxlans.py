2a699d87736cf2a03b5280f945f4edf8ad5fd146
Anthony Timmins <atimmins@datto.com>
2022-05-05 09:47:06 -0400

    Fix vxlan_bridge test

--- tests/generator/test_vxlans.py
+++ tests/generator/test_vxlans.py
@@ -36,9 +36,7 @@ class TestNetworkd(TestBase):
       vni: 1005''')
         self.assert_networkd({
           'vxlan1005.network':
-          '[Match]\nName=vxlan1005\n\n[Network]\nLinkLocalAddressing=ipv6\nConfigureWithoutCarrier=yes\nBridge=br1005\n',
-          'vxlan1005.netdev':
-          '[NetDev]\nName=vxlan1005\nKind=vxlan\n\n[VXLAN]\nVNI=1005'})
+          '[Match]\nName=vxlan1005\n\n[Network]\nLinkLocalAddressing=ipv6\nConfigureWithoutCarrier=yes\nBridge=br1005\n'})
 
     def test_vxlan_destinaton_port(self):
         self.generate('''network:
